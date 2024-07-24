f6756c00764fec5694efac2af5fe30bbf98aefa4
Zel-alemm <yalem7155@gmail.com>
2024-07-23 19:45:00 -1100

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+
+const slider = document.querySelector('.slider');
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('click', activate);
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    if (e.target.matches('.next')) {
+        slider.append(items[0]);
+    } else if (e.target.matches('.prev')) {
+        slider.prepend(items[items.length - 1]);
+    }
+}
+
+function displayText(id) {
+    var texts = document.getElementsByClassName("text-content");
+    for (var i = 0; i < texts.length; i++) {
+        texts[i].style.display = "none";
+    }
+    document.getElementById(id).style.display = "block";
+
+    var lis = document.querySelectorAll(".navmenu ul li");
+    lis.forEach(function (li) {
+        li.classList.remove("active");
+    });
+
+    document.getElementById("item" + id.charAt(4)).classList.add("active");
+}
+
+
+function openCity(evt, cityName, videoSrc) {
+    var i, tabcontent, tablinks;
+  
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove "active" class from all tablinks
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab content
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  
+    // Update the video source
+    var videoPlayer = document.getElementById("videoPlayer");
+    videoPlayer.src = videoSrc;
+    videoPlayer.load();  // Load the new video source
+    videoPlayer.play();  // Play the new video
+  }
+
+  
+
+  function openPage(pageName,elmnt) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink1");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
